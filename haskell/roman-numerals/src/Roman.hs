f4dfdbcac3bd210e563fdb9780b70dbcfb1215c3
junallan <junallan@telus.net>
2017-03-08 07:15:23 -0700

    exercism.io Roman Numeral problem
    Completed solution to Roman Numerals exercism.io problem

--- haskell/roman-numerals/src/Roman.hs
+++ haskell/roman-numerals/src/Roman.hs
@@ -31,10 +31,16 @@ romanNumeralLookup x
   | x > 10 && x < 50      = case x < 40 of
                               True  -> Just (concat ((replicate (x `div` 10) "X") ++ [(fromMaybe "" (numerals (toInteger (x `mod` 10))))] ))
                               False -> Just ("XL" ++ fromMaybe "" (numerals (toInteger (x `mod` 40))))
-  | x > 50 && x < 100     = Nothing 
-  | x > 100 && x < 500    = Nothing
-  | x > 500 && x < 1000   = Nothing
-  | x > 1000 && x <= 3000 = Nothing    
+  | x > 50 && x < 100     = case x < 90 of
+                              True  -> Just (concat ((replicate (x `div` 50) "L") ++ [(fromMaybe "" (numerals (toInteger (x `mod` 50))))] ))
+                              False -> Just ("XC" ++ fromMaybe "" (numerals (toInteger (x `mod` 90))))
+  | x > 100 && x < 500    = case x < 400 of
+                              True  -> Just (concat ((replicate (x `div` 100) "C") ++ [(fromMaybe "" (numerals (toInteger (x `mod` 100))))] ))
+                              False -> Just ("CD" ++ fromMaybe "" (numerals (toInteger (x `mod` 400))))
+  | x > 500 && x < 1000   = case x < 900 of
+                              True  -> Just (concat ((replicate (x `div` 500) "D") ++ [(fromMaybe "" (numerals (toInteger (x `mod` 500))))] ))
+                              False -> Just ("CM" ++ fromMaybe "" (numerals (toInteger (x `mod` 900))))
+  | x > 1000 && x <= 3000 = Just (concat ((replicate (x `div` 1000) "M") ++ [(fromMaybe "" (numerals (toInteger (x `mod` 1000))))] ))
   | otherwise             = Nothing 
  --where findRomanNumeralCombination pattern n =          
 --numerals  = error "You need to implement this function."
