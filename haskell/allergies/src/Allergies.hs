bdafd600131559970e5833ebf7c62cb77a5ba1d3
junallan <junallan@telus.net>
2017-03-18 16:26:40 -0600

    exercism.io allergies problem
    Revision to allergies problem to use Lambda case

--- haskell/allergies/src/Allergies.hs
+++ haskell/allergies/src/Allergies.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Allergies (Allergen(..), allergies, isAllergicTo) where
 
 import Data.Bits
@@ -16,16 +18,15 @@ allergies :: Int -> [Allergen]
 allergies x = filter (\allergen -> isAllergicTo allergen x) [Cats,Chocolate,Eggs,Peanuts,Pollen,Shellfish,Strawberries,Tomatoes]
 
 isAllergicTo :: Allergen -> Int -> Bool
-isAllergicTo a x = if (.&.) (allergenAsInt a) x /= 0 then True else False
+isAllergicTo a x = (.&.) (allergenAsInt a) x /= 0
 
 allergenAsInt :: Allergen -> Int
-allergenAsInt x = 
-  case x of 
-    Eggs -> 1
-    Peanuts -> 2
-    Shellfish -> 4
-    Strawberries -> 8
-    Tomatoes -> 16
-    Chocolate -> 32
-    Pollen -> 64
-    Cats -> 128
+allergenAsInt = \case  
+                  Eggs -> 1
+                  Peanuts -> 2
+                  Shellfish -> 4
+                  Strawberries -> 8
+                  Tomatoes -> 16
+                  Chocolate -> 32
+                  Pollen -> 64
+                  Cats -> 128
