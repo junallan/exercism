dce956951a49ec68d150ca870642f69dc7b2f2aa
junallan <junallan@telus.net>
2017-03-20 06:35:15 -0600

    exericm.io Largest Series Product problem
    Solution to Largest Series Product

--- haskell/largest-series-product/src/Series.hs
+++ haskell/largest-series-product/src/Series.hs
@@ -5,7 +5,6 @@ import Data.Text as T
 import Data.String (fromString)
 import Data.Char (digitToInt)
 
-
 largestProduct :: Int -> String -> Maybe Integer
 largestProduct 0 digits      = Just 1
 largestProduct series ""     = if series == 0 then Just 1 else Nothing
@@ -13,12 +12,9 @@ largestProduct series digits =
   case Prelude.length digits < series || series < 0 of
     True         -> Nothing
     False        -> let 
-                      --digit    = digitToInt x
                       isNumber = (Prelude.length $ Prelude.filter (\x -> digitToInt x < 0 || digitToInt x > 9) digits) == 0
                     in 
                       if isNumber then Just (toInteger . fst $ Prelude.maximum $ Prelude.map (\x -> (seriesProduct x,x)) (getAllSubstrings series digits series)) else Nothing
-
---    False        -> Just (toInteger . fst $ Prelude.maximum $ Prelude.map (\x -> (seriesProduct x,x)) (getAllSubstrings series digits series))
  
 getAllSubstrings :: Int -> String -> Int -> [String]
 getAllSubstrings series [] counter     = []
@@ -26,12 +22,13 @@ getAllSubstrings series digits 1       = getSubstrings series digits
 getAllSubstrings series digits counter = getSubstrings series digits ++ getAllSubstrings series (Prelude.tail digits) (counter - 1)
 
 seriesProduct :: String -> Int
-seriesProduct [] = 1
+{-seriesProduct [] = 1
 seriesProduct digits = 
   let 
     values = Prelude.map digitToInt digits
   in
-    Prelude.head values * (seriesProduct $ Prelude.tail digits)   
-
+    Prelude.head values * (seriesProduct $ Prelude.tail digits) -}  
+seriesProduct digits = Prelude.foldl (*) 1 (Prelude.map digitToInt digits)
+    
 getSubstrings :: Int -> String -> [String]
 getSubstrings series digits = Prelude.map (\y -> unpack y) (Prelude.filter (\x -> Prelude.length (unpack x) == series) (chunksOf series (fromString digits::Text)))
