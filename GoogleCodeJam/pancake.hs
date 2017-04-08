53506f1263089d46c319c4812ffbc859ecacc0d5
junallan <junallan@telus.net>
2017-04-08 16:08:56 -0600

    Pancake problem
    Handle case of "--+--" 3

--- GoogleCodeJam/pancake.hs
+++ GoogleCodeJam/pancake.hs
@@ -1,5 +1,6 @@
 import Data.List
 import Data.Ord
+import Data.Maybe
 
 pancake :: String -> Integer -> Integer -> String
 pancake s k numFlips
@@ -10,13 +11,59 @@ pancake s k numFlips
 patternSequence :: String -> [(Int, Char)]
 patternSequence xs = map (\s -> (length s, s!!0)) (group xs)
 
-maximumCharacterElement :: [(Int,Char)] -> Char -> Int
+maximumCharacterElement :: [(Int,Char)] -> Char -> ((Int,Char),Int)
 maximumCharacterElement s c = 
   let 
-    results = zip s [0..]
+    results = characterElements s
   in
-    snd $ maximumBy (comparing fst) (filter(\((repetition,value),index) -> value==c) results)
+    maximumBy (comparing fst) (filter(\((repetition,value),index) -> value==c) results)
 
+characterElements :: [(Int,Char)] -> [((Int,Char),Int)]
+characterElements xs = zip xs [0..]
+
+previousCharacterElement :: [((Int,Char),Int)] -> Int -> Maybe ((Int,Char),Int)
+previousCharacterElement xs index = if index - 1 >= 0 then Just (xs !! (index - 1)) else Nothing
+
+nextCharacterElement :: [((Int,Char),Int)] -> Int -> Maybe ((Int,Char),Int)
+nextCharacterElement xs index = if index + 1 < length xs then Just (xs !! (index + 1)) else Nothing
+
+flipPancake :: String -> Integer -> String
+flipPancake s k =
+  let
+    patternSeq                   = patternSequence s
+    maxCharElement               = maximumCharacterElement patternSeq '-'
+    ((maxRepetionCount,_),maxIndex) = maxCharElement
+    charElements                 = characterElements patternSeq
+    prevCharElement              = previousCharacterElement charElements  maxIndex
+    nextCharElement              = nextCharacterElement charElements maxIndex
+  in
+    if maxRepetionCount == fromInteger k 
+    then 
+      concat $ map (\((rep,val),_) -> replicate rep val) (map (\((repetion,value),indx) -> if maxIndex == indx then flipElement ((repetion,value),indx) else ((repetion,value),indx)) charElements)
+    else
+      case isNothing prevCharElement of
+        True  -> ""
+        False -> let
+                   prvCharElement              = fromJust prevCharElement
+                   prevChrElementRepetionCount = fst (fst prvCharElement)
+                   prevChrElement              = snd (fst prvCharElement)
+                   prevIndex                   = snd prvCharElement
+--                   ((prevCharElementRepetionCount, prevCharElement), prevIndex) = prevCharElement
+                 in
+                   if prevChrElementRepetionCount == fromInteger k - maxRepetionCount
+                   then
+                     concat $ map (\((rep,val),_) -> replicate rep val) (map (\((repetion,value),indx) -> if maxIndex == indx || prevIndex == indx then flipElement ((repetion,value),indx) else ((repetion,value),indx)) charElements)
+                   else
+                     ""
+
+flipElement :: ((Int,Char),Int) -> ((Int,Char),Int)
+flipElement ((repetion,value),index) =
+  let
+    flippedValue = if value == '-' then '+' else '-'
+  in
+    ((repetion,flippedValue),index)
+
+{-
 flipPancake :: String -> Integer -> String
 flipPancake s k =
   let
@@ -25,3 +72,4 @@ flipPancake s k =
     newVal       = if snd y == '-' then [(fst y,'+')] else [(fst y,'-')]
   in
     concat $ map (\(repetition,value) -> replicate repetition value) (x ++ newVal ++ ys)
+-}
