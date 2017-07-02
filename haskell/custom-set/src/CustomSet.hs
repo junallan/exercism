69b1260604d17a73bf61b0b814cbf3aa7bbb50d7
junallan <junallan@telus.net>
2017-07-02 15:14:48 -0600

    exercism.io CustomSet problem
    Refactored CustomSet problem to remove reduncies.

--- haskell/custom-set/src/CustomSet.hs
+++ haskell/custom-set/src/CustomSet.hs
@@ -20,16 +20,13 @@ import Data.List(intersect, (\\), nub)
 data CustomSet a = Empty | Item a (CustomSet a) deriving (Show)
 
 instance (Eq a) => Eq (CustomSet a) where
-  left == right  = (size left == size right) && (isSubsetOf left right)
+  left == right  = (size left == size right) && isSubsetOf left right
   
 delete :: (Eq a) => a -> CustomSet a -> CustomSet a
 delete x set = fromList $ toList set \\ [x]
 
 difference :: (Eq a) => CustomSet a -> CustomSet a -> CustomSet a
-difference Empty Empty = Empty
-difference Empty setB  = Empty
-difference setA  Empty = setA
-difference setA setB    = fromList $ toList setA \\ toList setB 
+difference setA setB   = fromList $ toList setA \\ toList setB 
 
 empty :: CustomSet a
 empty = Empty
@@ -39,44 +36,32 @@ fromList [] = Empty
 fromList set = Item (head set) (fromList (tail set))
 
 insert :: (Eq a) => a -> CustomSet a -> CustomSet a
-insert a Empty = fromList [a]
 insert a set   = if member a set then set else fromList $ a : toList set
 
 intersection :: (Eq a ) => CustomSet a -> CustomSet a -> CustomSet a
-intersection Empty Empty = Empty
-intersection Empty setB  = Empty
-intersection setA  Empty = Empty
 intersection setA setB   = fromList $ toList setA `intersect` toList setB
 
 isDisjointFrom :: (Eq a) => CustomSet a -> CustomSet a -> Bool
-isDisjointFrom Empty Empty = True
-isDisjointFrom setA Empty = True
-isDisjointFrom Empty setB = True
-isDisjointFrom (Item a (setA)) setB = if member a setB then False else isDisjointFrom setA setB
+isDisjointFrom Empty _            = True
+isDisjointFrom (Item a setA) setB = not (member a setB) && isDisjointFrom setA setB
 
 isSubsetOf :: (Eq a) => CustomSet a -> CustomSet a -> Bool
-isSubsetOf Empty Empty = True
-isSubsetOf Empty setB = True
-isSubsetOf setA Empty = False
-isSubsetOf (Item a (setA)) setB = if member a setB then isSubsetOf setA setB else False
+isSubsetOf Empty _            = True
+isSubsetOf (Item a setA) setB = member a setB && isSubsetOf setA setB 
 
 member :: (Eq a) => a -> CustomSet a -> Bool
 member a Empty = False
-member a (Item b (set)) = if a == b then True else member a set
+member a (Item b set) = (a == b) || member a set
 
 null :: (Eq a) => CustomSet a -> Bool
-null set = case set == Empty of True -> True
-                                False -> False 
+null set = set == Empty
 
 size :: (Eq a) => CustomSet a -> Int
 size set = length $ toList set
 
 toList :: (Eq a) => CustomSet a -> [a]
 toList Empty = []
-toList (Item a (set)) = nub $ a : toList set
+toList (Item a set) = nub $ a : toList set
 
 union :: (Eq a) => CustomSet a -> CustomSet a -> CustomSet a
-union Empty Empty = Empty
-union Empty setB  = setB
-union setA  Empty = setA
 union setA setB   = fromList (toList (difference setA setB) ++ toList (difference setB setA) ++ toList (intersection setA setB))
