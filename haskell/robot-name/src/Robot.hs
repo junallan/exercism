b8fa8ce30fb557c8ca74fcc37e422c6523fff013
junallan <junallan@telus.net>
2017-09-03 18:02:20 -0600

    exercism.io Robot Name problem
    Refactored Robot Name problem based on hlint recommendations.

--- haskell/robot-name/src/Robot.hs
+++ haskell/robot-name/src/Robot.hs
@@ -8,14 +8,14 @@ data Robot = Robot { name :: IORef String}
 mkRobot :: IO Robot
 mkRobot = do 
             generator <- newStdGen
-            let randomCode = (take 2 $ randomRs ('A','Z') generator) ++ (take 3 $ randomRs ('0','9') generator)
+            let randomCode = take 2 (randomRs ('A','Z') generator) ++ take 3 (randomRs ('0','9') generator)
             irefCode <- newIORef randomCode
             return (Robot irefCode)
  
 resetName :: Robot -> IO ()
 resetName (Robot name) = do
                            generator <- newStdGen
-                           let randomCode = (take 2 $ randomRs ('A','Z') generator) ++ (take 3 $ randomRs ('0','9') generator)
+                           let randomCode = take 2 (randomRs ('A','Z') generator) ++ take 3 (randomRs ('0','9') generator)
                            writeIORef name randomCode
 
 robotName :: Robot -> IO String
