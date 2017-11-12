c383b2dc443e70081cecc358376e9cbcab3c60f3
junallan <junallan@telus.net>
2017-11-12 14:45:14 -0700

    exercism.io Deque problem
    Just create, push, and pop functions complete with first test passing.

--- haskell/linked-list/src/Deque.hs
+++ haskell/linked-list/src/Deque.hs
@@ -2,31 +2,31 @@ module Deque (Deque, mkDeque, pop, push, shift, unshift) where
 
 import Data.IORef
 
---data Deque a = Dummy
-data Deque a = EmptyNode | Cell a (Deque a) (Deque a)
-
+type Deque a = IORef [a]
 --data BankAccount = BankAccount { balance :: IORef (Maybe Integer)}
 --EmptyList | Cell a (LinkedList a)
 
 mkDeque :: IO (Deque a)
-mkDeque = return EmptyNode
+mkDeque = newIORef []
 --mkDeque = error "You need to implement this function."
 
 pop :: Deque a -> IO (Maybe a)
-pop EmptyNode = return Nothing
-pop (Cell x _ _) = return (Just x)
+pop x = do
+                  items <- readIORef x
+                  if null items then 
+                    return Nothing 
+                  else 
+                    do 
+                    	writeIORef x (take ((length items)-1) items) 
+                    	return (Just (last items))
+--pop (Cell x _ _) = return (Just x)
 --pop deque = error "You need to implement this function."
 
 push :: Deque a -> a -> IO ()
-{-push EmptyNode x = do 
-	                 item <- newIORef EmptyNode
-	                 --modifyIORef item ((Cell x) EmptyNode EmptyNode)
-	                 writeIORef item ((Cell x) EmptyNode EmptyNode)-}
+push items x = do
+                 items' <- readIORef items
+                 writeIORef items (items' ++ [x])
 
-push deque x = do
-                 item <- newIORef deque
-                 item' <- readIORef item
-                 writeIORef item ((Cell x) EmptyNode item')
 
 unshift :: Deque a -> a -> IO ()
 unshift deque x = error "You need to implement this function."
