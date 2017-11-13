313ba59637fd48574023607e0e8f325f1f553727
junallan <junallan@telus.net>
2017-11-13 14:48:20 -0700

    exercism.io Linked List problem
    Solution to exercism.io Linked List problem

--- haskell/linked-list/src/Deque.hs
+++ haskell/linked-list/src/Deque.hs
@@ -2,34 +2,39 @@ module Deque (Deque, mkDeque, pop, push, shift, unshift) where
 
 import Data.IORef
 
-type Deque a = IORef [a]
---data BankAccount = BankAccount { balance :: IORef (Maybe Integer)}
---EmptyList | Cell a (LinkedList a)
+data Deque a = Deque (IORef [a])
 
 mkDeque :: IO (Deque a)
-mkDeque = newIORef []
---mkDeque = error "You need to implement this function."
+mkDeque = do
+            emptyItems <- newIORef []
+            return (Deque emptyItems)
 
 pop :: Deque a -> IO (Maybe a)
-pop x = do
+pop (Deque x) = do
                   items <- readIORef x
                   if null items then 
                     return Nothing 
                   else 
                     do 
-                    	writeIORef x (take ((length items)-1) items) 
-                    	return (Just (last items))
---pop (Cell x _ _) = return (Just x)
---pop deque = error "You need to implement this function."
+                      writeIORef x (take (length items-1) items) 
+                      return (Just (last items))
 
 push :: Deque a -> a -> IO ()
-push items x = do
-                 items' <- readIORef items
-                 writeIORef items (items' ++ [x])
-
+push (Deque x) val = do
+                       items <- readIORef x
+                       writeIORef x (items ++ [val])
 
 unshift :: Deque a -> a -> IO ()
-unshift deque x = error "You need to implement this function."
+unshift (Deque x) val = do
+                          items <- readIORef x
+                          writeIORef x (val : items)  
 
 shift :: Deque a -> IO (Maybe a)
-shift deque = error "You need to implement this function."
+shift (Deque x) = do
+                    items <- readIORef x
+                    if null items then
+                      return Nothing
+                    else
+                      do
+                        writeIORef x (tail items)
+                        return (Just (head items))
